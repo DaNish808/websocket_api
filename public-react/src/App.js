09c239306e66b8023dd09f400fe936a6a342a3bd
David Nishikawa <triptype@gmail.com>
2018-02-03 00:31:59 -0800

    set App contents to grid

--- public-react/src/App.js
+++ public-react/src/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
 import Settings from './components/Settings/Settings';
+import Members from './components/Members/Members';
 import Chat from './components/Chat/Chat';
 
 import './App.css';
@@ -15,6 +16,7 @@ class App extends PureComponent {
           <h1>hello</h1>
           <Settings/>
         </header>
+        <Members/>
         <Chat/>
       </div>
     );
