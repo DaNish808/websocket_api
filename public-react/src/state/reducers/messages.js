a92a01fdec41d2f442966ea10b5880aa08cf136e
David Nishikawa <triptype@gmail.com>
2018-02-03 02:00:43 -0800

    generate mock data to style

--- public-react/src/state/reducers/messages.js
+++ public-react/src/state/reducers/messages.js
@@ -1,8 +1,115 @@
-
 import { POST_ALL, RECEIVE_POST } from '../constants';
+import generateName from 'sillyname';
 
+const devDefaults = [
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  },
+  {
+    user: generateName(),
+    timestamp: new Date(),
+    text: Array(Math.floor(Math.random() * 40)).fill('').map(() => generateName())
+  }
+];
 
-export default function messages(state = [], { type, payload }) {
+export default function messages(state = devDefaults || [], { type, payload }) {
   switch(type) {
     case RECEIVE_POST:
     case POST_ALL:
